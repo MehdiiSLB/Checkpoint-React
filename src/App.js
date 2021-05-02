e51d0852eb7c63d9e9c994ac75d745d1cee8e16f
Mehdi <mido.622@outlook.fr>
2021-05-02 17:38:31 +0100

    checkpoint react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+// importer Jobs components
+import Jobs from "./components/Jobs";
+import Data from "./data.json";
+import { useState } from "react";
 
 function App() {
+  const [counter, setConter] = useState(0);
+
+  const inc = () => {
+    setConter(counter + 1);
+  };
+  const dec = () => {
+    setConter(counter - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={inc}>+</button>
+      <button onClick={dec}>-</button>
+
+      <div className="App">
+        <Jobs data={Data[counter]} />
+        {/* {Data.map((elem) => (
+        <Jobs data={elem} />
+      ))} */}
+      </div>
     </div>
   );
 }
